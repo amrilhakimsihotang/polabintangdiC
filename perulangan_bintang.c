98e72246d8f3efddceb2293c187e162e21c6e575
amrilhakimsihotang <68908992+amrilhakimsihotang@users.noreply.github.com>
2020-09-04 11:21:32 +0700

    Add files via upload

---
+++ perulangan_bintang.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    int x;
+    int y;
+    int k;
+
+    for(x=1;x<10;x++){
+        printf("\n");
+        for(y=0;y<x;y++){
+            printf("*");
+        }
+    }
+
+return 0;
+}
