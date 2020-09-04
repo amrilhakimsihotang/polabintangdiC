ed71f921de3d68011a30a1194ee37746e7b8632d
amrilhakimsihotang <68908992+amrilhakimsihotang@users.noreply.github.com>
2020-09-04 11:29:29 +0700

    Update perulangan_bintang.c

--- perulangan_bintang.c
+++ perulangan_bintang.c
@@ -6,10 +6,19 @@ int main(){
     int x;
     int y;
     int k;
+    int l;
 
-    for(x=1;x<10;x++){
+  for(x=1;x<10;x++){
         printf("\n");
         for(y=0;y<x;y++){
+            printf("*");
+        }
+    }
+
+
+    for(k=10;k>=1;k--){
+        printf("\n");
+        for(l=1;l<k;l++){
             printf("*");
         }
     }
